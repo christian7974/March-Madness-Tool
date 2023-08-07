041144020219d9b47a601c7ad6692d8d171d90ef
christian7974 <ctropeano@uri.edu>
2023-08-07 15:24:22 -0400

    More Skeleton Code
    Worked on the index.html, added really simple styling. Added post function.

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require('express');
 const https = require('https');
 const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"))
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    const team1 = req.body.team1;
+    const team2 = req.body.team2;
+    res.write(team1);
+    res.write(" " + team2);
+    res.send();
+
 });
 
 app.listen(3000, function() {
