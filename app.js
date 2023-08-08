9e33d43f8422bd0991a6cf8ac263119c96ee0c5e
christian7974 <ctropeano@uri.edu>
2023-08-08 16:41:30 -0400

    Before Big Change
    Want to stop myself from repeating code when I am writing the rows of the tables.

--- app.js
+++ app.js
@@ -10,19 +10,7 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 app.get("/", function(req, res) {
-    res.render("table", {
-        team1Name: "Team1",
-        team2Name: "Team2",
-
-        team1Points: "Team1",
-        team2Points: "Team2",
-
-        team1TotalReb: "Team1",
-        team2TotalReb: "Team2",
-        
-        team13PP: "Team1",
-        team23PP: "Team2",
-    });
+    res.render("index");
 });
 
 app.post("/", function(req, res) {
@@ -33,21 +21,66 @@ app.post("/", function(req, res) {
     const url = "https://college-basketball-api.onrender.com/teams/compare/" + team1 + "/" + team2;
     https.get(url, function(response) {
         response.on("data", function(data) {
-            // console.log(JSON.parse(data));
             firstTeam = JSON.parse(data)[0];
             secondTeam = JSON.parse(data)[1];
             res.render("table", {
                 team1Name: firstTeam.name,
                 team2Name: secondTeam.name,
 
-                team1Points: firstTeam.pointsPG ,
-                team2Points: secondTeam.pointsPG,
+                team1pointsPG: firstTeam.pointsPG,
+                team2pointsPG: secondTeam.pointsPG,
 
-                team1TotalReb: firstTeam.totalReboundsPG,
-                team2TotalReb: secondTeam.totalReboundsPG,
+                team1fieldGoalsMadePG: firstTeam.fieldGoalsMadePG,
+                team2fieldGoalsMadePG: secondTeam.fieldGoalsMadePG,
 
-                team13PP: firstTeam.threePointPercent,
-                team23PP: secondTeam.threePointPercent,
+                team1fieldGoalsAttPG: firstTeam.fieldGoalsAttPG,
+                team2fieldGoalsAttPG: secondTeam.fieldGoalsAttPG,
+
+                team1FGPercent: firstTeam.FGPercent,
+                team2FGPercent: secondTeam.FGPercent,
+
+                team1threePointMadePG: firstTeam.threePointMadePG,
+                team2threePointMadePG: secondTeam.threePointMadePG,
+
+                team1threePointAttPG: firstTeam.threePointAttPG,
+                team2threePointAttPG: secondTeam.threePointAttPG,
+
+                team1threePointPercent: firstTeam.threePointPercent,
+                team2threePointPercent: secondTeam.threePointPercent,
+
+                team1freeThrowMadePG: firstTeam.freeThrowMadePG,
+                team2freeThrowMadePG: secondTeam.freeThrowMadePG, 
+
+                team1freeThrowAttPG: firstTeam.freeThrowAttPG,
+                team2freeThrowAttPG: secondTeam.freeThrowAttPG, 
+
+                team1freeThrowPercent: firstTeam.freeThrowPercent,
+                team2freeThrowPercent: secondTeam.freeThrowPercent,
+
+                team1offReboundsPG: firstTeam.offReboundsPG,
+                team1offReboundsPG: firstTeam.offReboundsPG,
+
+                team1defReboundsPG: firstTeam.defReboundsPG,
+                team1defReboundsPG: firstTeam.defReboundsPG,
+
+                team1totalReboundsPG: firstTeam.totalReboundsPG,
+                team2totalReboundsPG: secondTeam.totalReboundsPG,
+
+                team1assistsPG: firstTeam.assistsPG,
+                team2assistsPG: secondTeam.assistsPG,
+
+                team1stealsPG: firstTeam.stealsPG,
+                team2stealsPG: secondTeam.stealsPG,
+
+                team1blocksPG: firstTeam.blocksPG,
+                team2blocksPG: secondTeam.blocksPG,
+
+                team1turnoversPG: firstTeam.turnoversPG,
+                team2turnoversPG: secondTeam.turnoversPG,
+
+                teamStats: ["pointsPG", "fieldGoalsMadePG", "fieldGoalsAttPG", "FGPercent", "threePointMadePG", 
+                "threePointAttPG", "threePointPercent", "freeThrowMadePG", "freeThrowAttPG", "freeThrowPercent", "offReboundsPG", 
+                "defReboundsPG", "totalReboundsPG", "assistsPG", "stealsPG", "blocksPG", "turnoversPG"]
             });
         });
     });
